40f6244abe8131bb6c40cf5bcf486161d96e9599
axiomaswn <yomaswn@gmail.com>
2017-05-05 20:10:26 +0700

    add option and table

--- weather/src/App.js
+++ weather/src/App.js
@@ -13,6 +13,28 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <select>
+          <option>Select City...</option>
+          <option>Jakarta</option>
+          <option>Tokyo</option>
+          <option>London</option>
+        </select>
+        <table>
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature</th>
+              <th>Variance</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>date</td>
+              <td>0C</td>
+              <td>0C</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }

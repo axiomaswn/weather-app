96cc3850a6dd0cf6f3258a9f72adae233e4b2945
axiomaswn <yomaswn@gmail.com>
2017-05-05 20:28:41 +0700

    wiring api

--- weather/src/App.js
+++ weather/src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      datas: [],
+    }
+  }
+  changeCity(e) {
+    const ct = e.target.value
+    const appThis = this
+    fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${ct}&mode=json&units=metric&cnt=5&APPID=481e3bc28e5264e5607c2b65b449bfc1`)
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      appThis.setState({
+        datas: data.list
+      })
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -13,11 +33,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <select>
-          <option>Select City...</option>
-          <option>Jakarta</option>
-          <option>Tokyo</option>
-          <option>London</option>
+        <select onChange={(e) => { this.changeCity(e) }}>
+          <option defaultValue=''>Select City...</option>
+          <option value='Jakarta'>Jakarta</option>
+          <option value='Tokyo'>Tokyo</option>
+          <option value='London'>London</option>
         </select>
         <table>
           <thead>
